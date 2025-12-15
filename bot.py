3890f71ac879ed2ca0dcf2a80d63fd7d886c8bff
awakevalo-spec <awakevalo@gmail.com>
2025-12-15 11:35:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
+import os
 import discord
 import asyncio
 import random
 
-# ==============================
-# HIER DEIN BOT-TOKEN EINTRAGEN
-# ==============================
-TOKEN = "DEIN TOKEN HIER"
+# Bot-Token kommt aus der Environment Variable (Render)
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-# Channel-ID, wo der Bot schreiben soll
+# Channel-ID, in den der Bot schreiben soll
 CHANNEL_ID = 1450051131811041280
 
 intents = discord.Intents.default()
@@ -17,13 +16,16 @@ async def send_periodic_message():
     await client.wait_until_ready()
     channel = client.get_channel(CHANNEL_ID)
 
+    if channel is None:
+        print("❌ Channel nicht gefunden. Prüfe die CHANNEL_ID.")
+        return
+
     while not client.is_closed():
         await channel.send(
             "🚨 **NEUES VIP VIDEO** 🚨\n\n"
-            "Es wurde **JETZT** gerade ein neues Video in der VIP gruppe hochgeladen.\n"
-            "Du bist noch nicht drinnen? 👉 "
-            "https://discord.com/channels/1436987920178479157/1447976453500047472\n\n"
-            
+            "Es wurde **JETZT** gerade ein neues Video in der VIP Gruppe hochgeladen.\n"
+            "Du bist noch nicht drinnen? 👉\n"
+            "https://discord.com/channels/1436987920178479157/1447976453500047472"
         )
 
         # 3–4 Minuten warten
